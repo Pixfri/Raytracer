90bf54b2a366e6d756efb5d406fe7b8403103b7c
Jean <jean.letessier@protonmail.com>
2024-08-16 16:39:18 +0200

    Logger Test.

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,14 @@
-#include "main.hpp"
+#include <Raytracer/Core/Logger.hpp>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
+    Raytracer::Logger::Init();
+
+    Raytracer::Log::RtTrace("Logger Test.");
+    Raytracer::Log::RtInfo("Logger Test.");
+    Raytracer::Log::RtWarn("Logger Test.");
+    Raytracer::Log::RtError("Logger Test.");
+    Raytracer::Log::RtCritical("Logger Test.");
+    Raytracer::Log::RtFatal({0x00, 0x00}, "Logger Test.");
 
     return 0;
 }
